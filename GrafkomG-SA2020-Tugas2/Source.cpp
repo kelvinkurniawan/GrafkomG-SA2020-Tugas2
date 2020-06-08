a9b53ae7e8823bfb5b24cb16a822b1eff3ef76e2
Kelvin Kurniawan <tepinnko@gmail.com>
2020-06-08 18:51:59 +0700

    Add line to tracking the plane

--- GrafkomG-SA2020-Tugas2/Source.cpp
+++ GrafkomG-SA2020-Tugas2/Source.cpp
@@ -7,7 +7,7 @@ int i;
 
 double pos = 0;
 
-void createPlanet(int rad, int point, int x_pos, int y_pos, float delta) {
+void createPlanet(int rad, int x_pos, int y_pos, float delta) {
 	glBegin(GL_POLYGON);
 	for (i = 0; i <= 360; i++) {
 		float A = (2 * PI) * i / 360;
@@ -18,28 +18,70 @@ void createPlanet(int rad, int point, int x_pos, int y_pos, float delta) {
 	glEnd();
 }
 
+
+void trackPlane(int x_pos, int y_pos, float delta) {
+	glBegin(GL_LINE_LOOP);
+	glColor3ub(60, 60, 60);
+	glVertex2f(100, 100);
+	for (i = 0; i <= 360; i++) {
+		float A = (2 * PI) * i / 360;
+		float x = cos(A) + (cos(pos * delta) * y_pos + x_pos);
+		float y = sin(A) + (sin(pos * delta) * y_pos + x_pos);
+		glVertex2f(x, y);
+	}
+	glEnd();
+}
+
+void createOrbit(int rad) {
+	int center = 100;
+	glColor3ub(60, 60, 60);
+	glBegin(GL_LINE_LOOP);
+	for (i = 0; i <= 360; i++) {
+		float A = (2 * PI) * i / 360;
+		float x = cos(A) * rad + center;
+		float y = sin(A) * rad + center;
+		glVertex2f(x, y);
+	}
+	glEnd();
+}
+
+
 void renderObject() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	// create orbit
+	createOrbit(20);
+	createOrbit(40);
+	createOrbit(60);
+	createOrbit(90);
+
+	
+
+	// create plane
+
 	// Sun
 	glColor3ub(241, 196, 15);
-	createPlanet(10, 100, 100, 0, 0);
+	createPlanet(10, 100, 0, 0);
 
 	// Mercury
 	glColor3ub(212, 158, 120);
-	createPlanet(2, 50, 100, 20, 20);
+	createPlanet(2, 100, 20, 20);
+	trackPlane(100, 20, 20);
 
 	// Venus
 	glColor3ub(236, 191, 150);
-	createPlanet(3, 100, 100, 40, 15);
+	createPlanet(3, 100, 40, 15);
+	trackPlane(100, 40, 15);
 
 	// Earth
 	glColor3ub(0, 179, 235);
-	createPlanet(5, 100, 100, 60, 10);
+	createPlanet(5, 100, 60, 10);
+	trackPlane(100, 60, 10);
 
 	// Mars
 	glColor3ub(222, 51, 32);
-	createPlanet(2.5, 100, 100, 90, 5);
+	createPlanet(2.5, 100, 90, 5);
+	trackPlane(100, 90, 5);
 
 	glFlush();
 }
