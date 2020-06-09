1bd7da09f3c93433778338ebf85343ac9c48451d
Kelvin Kurniawan <tepinnko@gmail.com>
2020-06-09 23:25:20 +0700

    Add function key

--- GrafkomG-SA2020-Tugas2/Source.cpp
+++ GrafkomG-SA2020-Tugas2/Source.cpp
@@ -7,8 +7,14 @@ int i;
 
 double pos = 0;
 double satellitePos = 0;
+double speed = 0;
 
 bool isMove = false;
+bool showOrbit = false;
+bool showSatellite = false;
+bool showSatelliteOrbit = false;
+bool showTrackPlane = false;
+bool showTrackSatellite = false;
 
 void createPlanet(int rad, int x_pos, int y_pos, float delta) {
 	glBegin(GL_POLYGON);
@@ -78,26 +84,35 @@ void createSatelliteOrbit(int rad, int x_pos, int y_pos, float delta) {
 
 
 void renderObject() {
+
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	// create orbit
-	createOrbit(20);
-	createOrbit(40);
-	createOrbit(60);
-	createOrbit(90);
+	if (showOrbit) {
+		createOrbit(20);
+		createOrbit(40);
+		createOrbit(60);
+		createOrbit(90);
+	}
 
-	trackPlane(100, 20, 20);
-	trackPlane(100, 40, 15);
-	trackPlane(100, 60, 10);
-	trackPlane(100, 90, 5);
+	if (showTrackPlane) {
+		trackPlane(100, 20, 20);
+		trackPlane(100, 40, 15);
+		trackPlane(100, 60, 10);
+		trackPlane(100, 90, 5);
+	}
 
-	createSatelliteOrbit(12, 100, 60, 10);
-	createSatelliteOrbit(5, 100, 90, 5);
-	createSatelliteOrbit(10, 100, 90, 5);
+	if (showSatelliteOrbit) {
+		createSatelliteOrbit(12, 100, 60, 10);
+		createSatelliteOrbit(5, 100, 90, 5);
+		createSatelliteOrbit(10, 100, 90, 5);
+	}
 
-	createSatellite(2, 100, 60, 10, 5, 12);
-	createSatellite(1, 100, 90, 5, 4.5, 5);
-	createSatellite(1, 100, 90, 5, 5, 10);
+	if (showSatellite) {
+		createSatellite(2, 100, 60, 10, 5, 12);
+		createSatellite(1, 100, 90, 5, 4.5, 5);
+		createSatellite(1, 100, 90, 5, 5, 10);
+	}
 	// create plane
 
 	// Sun
@@ -125,8 +140,8 @@ void renderObject() {
 
 void animate(int) {
 	if (isMove) {
-		pos += 0.00075f;
-		satellitePos += 0.005f;
+		pos += 0.00075f + speed;
+		satellitePos += 0.005f + speed;
 	}
 	else {
 		pos += 0.0f;
@@ -146,6 +161,59 @@ void key(unsigned char key, int x, int y) {
 			isMove = false;
 		}
 	}
+
+	if (key == '1') {
+		if (!showOrbit) {
+			showOrbit = true;
+		}
+		else {
+			showOrbit = false;
+		}
+	}
+
+	if (key == '2') {
+		if (!showTrackPlane) {
+			showTrackPlane = true;
+		}
+		else {
+			showTrackPlane = false;
+		}
+	}
+
+	if (key == '3') {
+		if (!showSatellite) {
+			showSatellite = true;
+		}
+		else {
+			showSatellite = false;
+		}
+	}
+
+	if (key == '4') {
+		if (!showSatelliteOrbit) {
+			showSatelliteOrbit = true;
+		}
+		else {
+			showSatelliteOrbit = false;
+		}
+	}
+
+	if (key == '5') {
+		if (!showTrackSatellite) {
+			showTrackSatellite = true;
+		}
+		else {
+			showTrackSatellite = false;
+		}
+	}
+
+	if (key == '+') {
+		speed += 0.00025;
+	}
+
+	if (key == '_') {
+		speed -= 0.00025;
+	}
 }
 
 int main(int argc, char** argv) {
@@ -159,6 +227,15 @@ int main(int argc, char** argv) {
 	glutDisplayFunc(renderObject);
 	glutKeyboardFunc(key);
 	gluOrtho2D(0, 200, 0, 200);
+	
+	std::cout << "--- How To Play --" << std::endl << std::endl;
+	std::cout << "- Press P to start/pause" << std::endl;
+	std::cout << "- Press 1 to show/hide orbit" << std::endl;
+	std::cout << "- Press 2 to enable/disable tracking the plane" << std::endl;
+	std::cout << "- Press 3 to show/hide satellite" << std::endl;
+	std::cout << "- Press 4 to show/hide satellite orbit" << std::endl;
+	std::cout << "- Press 5 to enable/disable tracking the satellite" << std::endl;
+	std::cout << "- Press +/- to increase/decrease speed" << std::endl;
 
 	glutMainLoop();
 
